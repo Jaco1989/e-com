0169fc12310fec492ec55a21d52f21a5f92e7c7c
Jaco <thiartjaco518@gmail.com>
2024-07-05 21:57:08 +0200

    setup redux

--- client/src/index.js
+++ client/src/index.js
@@ -9,9 +9,10 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
-import { HelmetProvider } from "react-helmet-async";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
+import store from "./store";
+import {Provider} from "react-redux"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -24,10 +25,8 @@ const router = createBrowserRouter(
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <HelmetProvider>
+    <Provider store={store}>
       <RouterProvider router={router} />
-    </HelmetProvider>
-  </React.StrictMode>
+    </Provider>
 );
 
