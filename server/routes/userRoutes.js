19350fa9d104068af53a74412a40da495a31ce99
Jaco <thiartjaco518@gmail.com>
2024-07-11 14:29:04 +0200

    set authenticating ser routes apply middleware to routes, securing routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,10 +3,11 @@ const router = express.Router();
 import {
     authUser, registerUser, logoutUser, getUserProfile, updateUserProfile, getUsers, getUserById, deleteUser, updateUser
 } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 router.route("/").post(registerUser).get(getUsers)
 router.post("/login", authUser)
-router.route("/profile").get(getUserProfile).put(updateUserProfile)
+router.route("/profile").get(protect, getUserProfile).put(protect, updateUserProfile)
 router.post("/logout", logoutUser)
 
 router.route("/:id").delete(deleteUser).get(getUserById).put(updateUser)
