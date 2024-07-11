9fc8330e5b121fc1075269c51e50b840369764fc
Jaco <thiartjaco518@gmail.com>
2024-07-11 11:04:39 +0200

    set user controller file

---
+++ server/controllers/userController.js
@@ -0,0 +1,40 @@
+import asyncHandler from "../middleware/asyncHandler.js";
+import User from "../models/userModel.js";
+
+const authUser = asyncHandler(async (req, res) => {
+    res.send("auth")
+  });
+
+const registerUser = asyncHandler(async (req, res) => {
+    res.send("register")
+  });
+
+const logoutUser = asyncHandler(async (req, res) => {
+    res.send("logout")
+  });
+
+const getUserProfile = asyncHandler(async (req, res) => {
+    res.send("get user profile")
+  });
+
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.send("update user profile")
+  });
+
+const getUsers = asyncHandler(async (req, res) => {
+    res.send("get users")
+  });
+
+const getUserById = asyncHandler(async (req, res) => {
+    res.send("get user id")
+  });  
+
+const deleteUser = asyncHandler(async (req, res) => {
+    res.send("delete")
+  });
+
+const updateUser = asyncHandler(async (req, res) => {
+    res.send("update user")
+  });
+
+export {authUser, registerUser, logoutUser, getUserProfile, updateUserProfile, getUsers, getUserById, deleteUser, updateUser}
