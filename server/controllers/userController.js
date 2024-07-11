88382389eea30a890e71a34a246d24853831a8a9
Jaco <thiartjaco518@gmail.com>
2024-07-11 13:03:18 +0200

    set jwt for user auth & set in http cookie

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,18 +1,32 @@
 import asyncHandler from "../middleware/asyncHandler.js";
 import User from "../models/userModel.js";
+import jwt from "jsonwebtoken"
+
 
 const authUser = asyncHandler(async (req, res) => {
     const {email, password} = req.body
 
     const user = await User.findOne({email})
+
     if(user && (await user.matchPassword(password))){
+      const token = jwt.sign({userId: user._id}, process.env.JWT_SECRET, {expiresIn: "30d"})
+
+      res.cookie("jwt", token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development" ,
+        sameSite: "strict",
+        magAge: 30 * 24 * 60 * 60 * 1000 // 30days
+      })
+
       res.json({
         _id: user._id,
         name: user.name,
         email: user.email,
         isAdmin: user.isAdmin
       })
-    }else{
+    }
+
+    else{
       res.status(401)
       throw new Error("Invalid Email or Password")
     }
