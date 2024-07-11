61f55c23b74ebfb62e4bc5780f56b86449e40ded
Jaco <thiartjaco518@gmail.com>
2024-07-11 12:18:34 +0200

    ordering code

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -27,11 +27,6 @@ const userSchema = mongoose.Schema(
   }
 );
 
-// Match user entered password to hashed password in database
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
 // Encrypt password using bcrypt
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
@@ -41,6 +36,11 @@ userSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
+
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
