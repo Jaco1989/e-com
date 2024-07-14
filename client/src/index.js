9a8bdde8eebc957d07d469e5e715ee44a4b112f0
Jaco <thiartjaco518@gmail.com>
2024-07-14 14:54:05 +0200

    set shipping screen and private routes

--- client/src/index.js
+++ client/src/index.js
@@ -9,22 +9,32 @@ import {
   Route,
   RouterProvider,
 } from "react-router-dom";
+import PrivateRoute from './components/PrivateRoute';
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from './screens/CartScreen';
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from './screens/RegisterScreen';
+import ShippingScreen from './screens/ShippingScreen';
 import store from "./store";
 import {Provider} from "react-redux"
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
+
+      {/* Public Routes */}
       <Route index={true} path="/" element={<HomeScreen />} />
       <Route path="/product/:id" element={<ProductScreen />} />
       <Route path='/cart' element={<CartScreen />} />
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
+
+      {/* Private Routes */}
+      <Route path='' element={<PrivateRoute/>}>
+        <Route path='/shipping' element={<ShippingScreen />} />
+      </Route>
+      
     </Route>
   )
 );
