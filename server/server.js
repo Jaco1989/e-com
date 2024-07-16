94cb2a2c6f3ae0404c85b4cdc911d1f78cdbd16e
Jaco <thiartjaco518@gmail.com>
2024-07-16 13:12:05 +0200

    set paypal api and update order to pay api

--- server/server.js
+++ server/server.js
@@ -23,6 +23,10 @@ app.use("/api/products", productRoutes);
 app.use("/api/users", UserRouter);
 app.use('/api/orders', orderRoutes);
 
+app.get('/api/config/paypal', (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
+
 
 app.use(notFound);
 app.use(errorHandler);
