78cda18b4262b60d66c651ca2da008e50a4ef8cd
Jaco <thiartjaco518@gmail.com>
2024-07-17 21:56:08 +0200

    set getUsers admin mutation

--- client/src/slices/userApiSlice.js
+++ client/src/slices/userApiSlice.js
@@ -2,44 +2,53 @@ import { USERS_URL } from '../constants';
 import { apiSlice } from './apiSlice';
 
 export const usersApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-      // SetCredentials => from authSlice => Form and Button Click
-      login: builder.mutation({
-        query: (data) => ({
-          url: `${USERS_URL}/auth`,
-          method: 'POST',
-          body: data,
-        }),
-        keepUnusedDataFor: 5,
-        providesTags: ["users"]
+  endpoints: (builder) => ({
+    // SetCredentials => from authSlice => Form and Button Click
+    login: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/auth`,
+        method: 'POST',
+        body: data,
       }),
-      register: builder.mutation({
-        query: (data) => ({
-          url: `${USERS_URL}`,
-          method: 'POST',
-          body: data,
-        }),
-      }),
-      profile: builder.mutation({
-        query: (data) => ({
-          url: `${USERS_URL}/profile`,
-          method: 'PUT',
-          body: data,
-        }),
-      }),
-      // logout => from authSlice => Button Click => Destroy cookie
-      logout: builder.mutation({
-        query: () => ({
-          url: `${USERS_URL}/logout`,
-          method: 'POST',
-        }),
+      keepUnusedDataFor: 5,
+      providesTags: ["users"]
+    }),
+    register: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}`,
+        method: 'POST',
+        body: data,
       }),
     }),
+    profile: builder.mutation({
+      query: (data) => ({
+        url: `${USERS_URL}/profile`,
+        method: 'PUT',
+        body: data,
+      }),
+    }),
+    // logout => from authSlice => Button Click => Destroy cookie
+    logout: builder.mutation({
+      query: () => ({
+        url: `${USERS_URL}/logout`,
+        method: 'POST',
+      }),
+    }),
+    getUsers: builder.query({
+      query: () => ({
+        url: USERS_URL,
+      }),
+      providesTags: ['users'],
+      keepUnusedDataFor: 5,
+    }),
+    
+  }),
 })
 
 export const {
     useLoginMutation,
     useLogoutMutation,
     useRegisterMutation,
-    useProfileMutation} 
+    useProfileMutation,
+    useGetUsersQuery} 
 = usersApiSlice
