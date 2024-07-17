3cfd987f02bcbe210fe867844216607e638fc343
Jaco <thiartjaco518@gmail.com>
2024-07-17 14:23:26 +0200

    set backend image multer upload route

--- server/server.js
+++ server/server.js
@@ -4,11 +4,13 @@ dotenv.config();
 import connectDB from "./config/db.js";
 connectDB();
 
+import path from 'path';
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 import productRoutes from "./routes/productRoutes.js";
 import orderRoutes from './routes/orderRoutes.js';
+import uploadRoutes from './routes/uploadRoutes.js';
 import { UserRouter } from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
@@ -22,10 +24,15 @@ app.use(cors());
 app.use("/api/products", productRoutes);
 app.use("/api/users", UserRouter);
 app.use('/api/orders', orderRoutes);
+app.use('/api/upload', uploadRoutes);
+
 
 app.get('/api/config/paypal', (req, res) =>
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
 );
+
+const __dirname = path.resolve();
+  app.use('/uploads', express.static(path.join(__dirname, "/uploads")));
 
 
 app.use(notFound);
